1dc204c962e2c1202cc8b1f9283ee6a17e9376ae
Peter Vinnicombe <pete.vinnicombe@tiltdigital.co.uk>
2019-02-19 14:45:11 +0000

    updated db details

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'test');
+define('DB_NAME', 'admin_tiltengine');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'tiltengineuser');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '76Rfj2@c');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
